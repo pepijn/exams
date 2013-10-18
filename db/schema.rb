57aaeb14497dff700b56a9fb1dc5e96b324213fc
Pepijn Looije <pepijn@looije.me>
2013-10-18 15:14:05 +0200

    timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131018120048) do
+ActiveRecord::Schema.define(version: 20131018131249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,8 +50,10 @@ ActiveRecord::Schema.define(version: 20131018120048) do
   end
 
   create_table "sessions", force: true do |t|
-    t.integer "question_stack", default: [],              array: true
-    t.integer "user_id",                     null: false
+    t.integer  "question_stack", default: [],              array: true
+    t.integer  "user_id",                     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
