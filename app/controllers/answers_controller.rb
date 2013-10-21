0a7fbf93f8ae331c5aa12d25f6e82499cf123b92
Pepijn Looije <pepijn@looije.me>
2013-10-21 22:14:12 +0200

    fix

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -20,13 +20,15 @@ class AnswersController < ProtectedController
     session = current_user.sessions.last
 
     if @answer.correct?
-      session.question_stack = session.question_stack[1..-1]
+      session.question_stack = session.question_stack[1..-1].compact
     elsif !session.question_stack[1..-1].include? @answer.question.id
       session.question_stack.insert rand(8..12), @answer.question.id
     end
 
     session.question_stack_will_change!
     session.save!
+
+    return redirect_to :root if session.question_stack.empty?
     redirect_to new_question_answer_path(session.question_stack.first)
   end
 
