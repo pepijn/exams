9d59f6ed54b7287cea21465e5151f78214993bda
Pepijn Looije <pepijn@looije.me>
2014-03-16 09:47:28 +0100

    fix

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,7 +1,12 @@
 class AlertsController < ProtectedController
   def create
     @question = Question.find params[:question_id]
-    @alert = @question.alerts.create user: current_user
+    @alert = @question.alerts.build
+    @alert.user = current_user
+    @alert.message = params[:alert][:message]
+    @alert.save
+
     redirect_to :back
   end
 end
+
